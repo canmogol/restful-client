9771bfe1d4981c59cb2b040485e8f70dd5cc5f52
Can A. MOGOL <canmogol@gmail.com>
2015-10-18 18:17:49 +0300

    request response example for user (single entity CRUD)

--- src/main/java/com/restful/client/Client.java
+++ src/main/java/com/restful/client/Client.java
@@ -1,21 +1,24 @@
 package com.restful.client;
 
 
-import com.fererlab.restful.CityResource;
-import com.fererlab.service.dto.CityIdIntegerDTO;
-import com.fererlab.service.dto.CityIdLongDTO;
+import com.fererlab.city.dto.CityIdIntegerDTO;
+import com.fererlab.city.dto.CityIdLongDTO;
+import com.fererlab.city.restful.CityResource;
+import com.fererlab.user.dto.UserDTO;
+import com.fererlab.user.restful.UserResource;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 public class Client {
 
-    private String url = "http://localhost:8080/restful-server-city/api/";
+    private String url = "http://localhost:8080/restful-server/api/";
 
     public static void main(String[] args) {
         Client client = new Client();
-        client.call();
+        client.runMultipleEntityTypeExample();
+        client.runBlockingRequestResponseExample();
     }
 
-    private void call() {
+    private void runMultipleEntityTypeExample() {
         // get a resource to call
         CityResource resource = Resources.create(CityResource.class, url);
 
@@ -26,6 +29,32 @@ public class Client {
         // get city with long type id
         CityIdLongDTO cityIdLongDTO = resource.createModelWithIDNoAudit();
         System.out.println(ToStringBuilder.reflectionToString(cityIdLongDTO));
+    }
+
+    private void runBlockingRequestResponseExample() {
+        // get a resource to call
+        UserResource resource = Resources.create(UserResource.class, url);
+
+        // create a user
+        UserDTO createUserDTO = new UserDTO();
+        createUserDTO.setUsername("john");
+        createUserDTO.setPassword("123");
+        UserDTO newUserDTO = resource.create(createUserDTO);
+        System.out.println(ToStringBuilder.reflectionToString(newUserDTO));
+
+        // find the created user
+        UserDTO foundUserDTO = resource.find(newUserDTO.getId());
+        System.out.println(ToStringBuilder.reflectionToString(foundUserDTO));
+
+        // update user
+        foundUserDTO.setUsername("Mike");
+        UserDTO updatedUserDTO = resource.update(foundUserDTO);
+        System.out.println(ToStringBuilder.reflectionToString(updatedUserDTO));
+
+        // delete user
+        UserDTO deletedUserDTO = resource.delete(updatedUserDTO.getId());
+        System.out.println(ToStringBuilder.reflectionToString(deletedUserDTO));
+
     }
 
 }
